f4e75eb8c43f902f918e1c30e1d8f8147a6f70b4
Hasibul Hasan <hasibulhasan442@gmail.com>
2022-03-14 21:32:01 +0600

    Dashboard

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -2,7 +2,24 @@
 @section('content')
 <div class="content">
     <div class="container-fluid">
-      xcvcvxcvxc xcvc vcxvx
+      <div class="row">
+          <div class="col-md-4">
+              <div class="card">
+                  <div class="card-body">
+                      <h2 class="text-center">Total Services</h2>
+                      <h5 class="text-center">{{ App\Models\Categories::get()->count() }}</h5>
+                  </div>
+              </div>
+          </div>
+          <div class="col-md-4">
+              <div class="card">
+                  <div class="card-body">
+                      <h2 class="text-center">Total User</h2>
+                      <h5 class="text-center">{{ App\Models\User::where("user_type",'customer')->get()->count() }}</h5>
+                  </div>
+              </div>
+          </div>
+      </div>
     </div>
 </div>
 @endsection
